24aacdb8821c404793cfa8f13d28874d1693e3aa
nilgonzalez07 <106734349+nilgonzalez07@users.noreply.github.com>
2022-06-09 11:01:07 +0200

    Update main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,35 @@
-game.splash("Benvinguts a la nau...", "\"LLUPIS-17\"")
-effects.starField.startScreenEffect()
-let mySprite = sprites.create(assets.image`LLUPIS-17`, SpriteKind.Player)
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    projectile = sprites.createProjectileFromSprite(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, mySprite, 50, 50)
+})
+let projectile: Sprite = null
+let mySprite: Sprite = null
+game.splash("Benvingut!", "Tenim una nova missió")
+game.splash("El nostre objectiu és...")
+game.splash("Ajudar al nostre amic \"8\" ", "a salvar l'espai")
+mySprite = sprites.create(assets.image`8`, SpriteKind.Player)
+mySprite.setPosition(80, 120)
+controller.moveSprite(mySprite, 100, 100)
+mySprite.setStayInScreen(true)
+info.setLife(3)
+game.onUpdate(function () {
+    projectile = sprites.createProjectileFromSide(assets.image`Asteroide`, 0, 50)
+    projectile.x += randint(0, scene.screenWidth())
+    projectile.setKind(SpriteKind.Enemy)
+})
